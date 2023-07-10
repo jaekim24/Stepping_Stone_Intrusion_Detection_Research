ae116d81d028d0772b9b3d4cda097669d5f6394f
jaekim24 <62858192+jaekim24@users.noreply.github.com>
2023-07-10 14:52:47 -0400

    added get rtt.txt

--- get_just_send_echo_and_timestamps.py
+++ get_just_send_echo_and_timestamps.py
@@ -1,6 +1,43 @@
 import os 
 import glob
 
+
+
+
+#get rtt.txt with just time no dups
+#create a list with the 
+def just_get_rtt():
+    rtt = []
+    for out in glob.glob('Cluster/*/*/*/*' ):
+        rtt_file = open(out[0:31]+ "rtt.txt",'w')
+        print(out[0:30]+"rtt.txt")
+        if(out.find("cluster.txt")):
+            file = open(out,'r')
+            #skips first line 
+            file.readline()
+            for line in file:
+                print(line)
+                if( line.find("*")):
+                    print("clsoing")
+                    file.close()
+                    break
+                else:
+                    #gets the rtt from cluster.txt
+                   # rtt_file.write(line[0:14])
+                   print(line[0:14])
+
+    rtt_file.close()
+
+
+
+
+
+
+
+
+
+            
+
 #need to access in a nested for loop 
 # for each ( attack_x):
 #     for each ( out_y ):
@@ -10,7 +47,7 @@ import glob
 #                 /echo/attack1/aws1/test1/only_echo_packets.txt
 #                 /echo/attack1/aws1/test1/only_echo_timestamps.txt
 
-
+#cluster/attacker1/aws1/test01/insert send and echo timestamps
 def traversing_the_directories():
     for out in glob.glob('coding_programs/analyzing_packets/*/*' ):
         if ("OUT" in out):
@@ -45,7 +82,8 @@ def removing_ack_packets(file_directory):
 
     for a_line in file_dir:
         #if the packet is not ack then add it to the list of non ack packets
-        if ( "[.]" not in a_line):
+        #also have to account for finish packets "[F.]"
+        if ( "[.]" not in a_line and "[F.]" not in a_line):
             list_of_send_and_echo_packets.append(a_line)
     #lines.close()  dont need to make a file anymore 
     return list_of_send_and_echo_packets
@@ -102,26 +140,43 @@ def seperate_to_just_send_file_and_echo_file(list_of_send_and_echo_packets, curr
     if not os.path.exists("send/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)):
         os.makedirs("send/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num))
 
-    just_send_packets = open ("send/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)+"/" +"send_packets.txt", "w")
+    just_send_packets = open ("send/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)+"/" +"send_packets.txt", "w+")
     # making a file by extracting the send packets from the mixed_data_file
     # is a send: when the first IP address doesnt end with a 22 
 
-    #stopped here 
-    with open ("send/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)+"/" +"send_timestamps.txt", "w") as send_timestamp_file:
+    total_send_packets = 0 
+
+    with open ("send/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)+"/" +"send_timestamps.txt", "w+") as send_timestamp_file:
+
+        no_blank_line_send_timestamp = []
+
         for mixed_packet in mixed_data_file:
             # if destination IP address uses the 22 port then it is a send packet
-            if ( mixed_packet.find('>') < mixed_packet.find('.22:')):
+            if ( mixed_packet.find('>') < mixed_packet.find('.22:') ):
+                total_send_packets += 1 
 
                 #if send packet insert to just_send_packets
                 #making just_send_packet file
                 just_send_packets.write(mixed_packet)       
 
                 #creating send_timestamp file 
-                send_timestamp = get_timestamp(mixed_packet)
-                send_timestamp_file.write(remove_the_dot(send_timestamp)+"\n")
+                send_timestamp = remove_the_dot(get_timestamp(mixed_packet))
+                
+                no_blank_line_send_timestamp.append(send_timestamp)
+                no_blank_line_send_timestamp.append('\n')
+                # send_timestamp_file.write(remove_the_dot(send_timestamp)+"\n")
+            
+            
+        #remove blank line at the end
+        for x in range(len(no_blank_line_send_timestamp)-1):
+            send_timestamp_file.write(no_blank_line_send_timestamp[x])
 
+        
     send_timestamp_file.close()
     just_send_packets.close()
+
+
+
 
     if not os.path.exists("echo/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)):
         os.makedirs("echo/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num))
@@ -130,6 +185,9 @@ def seperate_to_just_send_file_and_echo_file(list_of_send_and_echo_packets, curr
     just_echo_packets = open("echo/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)+"/" +"echo_packets.txt", "w")
 
     # is an echo: when the first IP address and if it ends in 22 it is an echo bc it came from the ssh server port
+
+    no_blank_line_echo_timestamp = []
+
     with open ("echo/" + str(name_of_which_attack) +"/"+ str(name_of_which_aws_or_cct)+"/"+str(name_of_which_test_num)+"/" +"echo_timestamps.txt", "w") as echofile:
         for mixed_packet_of_echo_and_send in mixed_data_file_:
             # if source IP address uses the 22 port then it is an echo packet
@@ -137,8 +195,16 @@ def seperate_to_just_send_file_and_echo_file(list_of_send_and_echo_packets, curr
             if ( mixed_packet_of_echo_and_send.find('>') > mixed_packet_of_echo_and_send.find('.22 ') & mixed_packet_of_echo_and_send.find('.22 ') != -1 ):
                 #if echo packet insert to just_echo_packets
                 just_echo_packets.write(mixed_packet_of_echo_and_send)
-                echo_timestamp = get_timestamp(mixed_packet_of_echo_and_send)
-                echofile.write(remove_the_dot(echo_timestamp)+"\n")
+                echo_timestamp = remove_the_dot(get_timestamp(mixed_packet_of_echo_and_send))
+                #echofile.write(remove_the_dot(echo_timestamp)+"\n")
+
+                no_blank_line_echo_timestamp.append(echo_timestamp)
+                no_blank_line_echo_timestamp.append('\n')
+
+        #remove blank line at the end
+        for x in range(len(no_blank_line_echo_timestamp)-1):
+            echofile.write(no_blank_line_echo_timestamp[x])
+
 
     echofile.close()
     just_echo_packets.close()
@@ -156,8 +222,11 @@ def main():
     #removing_ack_packets()
     #outputs a new file with the packets from the list 
     #creating_new_file_without_ack_packets()
-    traversing_the_directories()
     #seperate_to_just_send_file_and_echo_file()
+    #traversing_the_directories()
+    just_get_rtt()
+
+
 
 main()
 
